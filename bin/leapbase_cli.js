4029463e88e472ebe7b75e420052d70589662c3a
leapbase <yliu0@leapbase.com>
2019-08-26 19:58:43 -0400

    use args to manage cli commands

--- bin/leapbase_cli.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var util = require('util');
-var path = require('path');
-var _ = require('lodash');
-
-export function cli(args) {
-  console.log('>>> leapbase CLI');
-  console.log(args);
-}
